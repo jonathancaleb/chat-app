df7fe00e3dee4242843c3b71201a75eff0d65b99
jonathancaleb <jonathancaleb230@gmail.com>
2023-12-20 17:52:55 +0300

    add

--- lib/modules/frame/signin/view.dart
+++ lib/modules/frame/signin/view.dart
@@ -24,43 +24,46 @@ class SignInPage extends GetView<SignInController> {
   }
 
   Widget _buildThirdyPartyLogin(String loginType, String logo) {
-    return Container(
-      padding: EdgeInsets.all(10.h),
-      margin: EdgeInsets.only(bottom: 15.h),
-      width: 295.w,
-      height: 44.h,
-      decoration: BoxDecoration(
-          color: AppColors.primaryBackground,
-          borderRadius: const BorderRadius.all(Radius.circular(5)),
-          boxShadow: [
-            BoxShadow(
-                color: Colors.grey.withOpacity(0.1),
-                spreadRadius: 1,
-                blurRadius: 2,
-                offset: const Offset(0, 1))
-          ]),
-      child: Row(
-        mainAxisAlignment:
-            logo == '' ? MainAxisAlignment.center : MainAxisAlignment.start,
-        children: [
-          logo == ''
-              ? Container()
-              : Container(
-                  padding: EdgeInsets.only(left: 40.w, right: 30.w),
-                  child: Image.asset("assets/icons/$logo.png"),
-                ),
-          Container(
-            child: Text(
-              "sign in with $loginType",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                  color: AppColor.primaryText,
-                  fontWeight: FontWeight.normal,
-                  fontSize: 14.sp),
-            ),
-          )
-        ],
+    return GestureDetector(
+      child: Container(
+        padding: EdgeInsets.all(10.h),
+        margin: EdgeInsets.only(bottom: 15.h),
+        width: 295.w,
+        height: 44.h,
+        decoration: BoxDecoration(
+            color: AppColors.primaryBackground,
+            borderRadius: const BorderRadius.all(Radius.circular(5)),
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.grey.withOpacity(0.1),
+                  spreadRadius: 1,
+                  blurRadius: 2,
+                  offset: const Offset(0, 1))
+            ]),
+        child: Row(
+          mainAxisAlignment:
+              logo == '' ? MainAxisAlignment.center : MainAxisAlignment.start,
+          children: [
+            logo == ''
+                ? Container()
+                : Container(
+                    padding: EdgeInsets.only(left: 40.w, right: 30.w),
+                    child: Image.asset("assets/icons/$logo.png"),
+                  ),
+            Container(
+              child: Text(
+                "sign in with $loginType",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: AppColor.primaryText,
+                    fontWeight: FontWeight.normal,
+                    fontSize: 14.sp),
+              ),
+            )
+          ],
+        ),
       ),
+      onTap: () {},
     );
   }
 
@@ -89,6 +92,32 @@ class SignInPage extends GetView<SignInController> {
     );
   }
 
+  Widget _buildSignupWidget() {
+    return GestureDetector(
+      child: Column(
+        children: [
+          Text(
+            "Already have an account",
+            style: TextStyle(
+                color: AppColors.primaryText,
+                fontWeight: FontWeight.normal,
+                fontSize: 12.sp),
+          ),
+          GestureDetector(
+            child: Text(
+              "Sign up here",
+              style: TextStyle(
+                  color: AppColors.primaryElement,
+                  fontWeight: FontWeight.normal,
+                  fontSize: 12.sp),
+            ),
+          )
+        ],
+      ),
+      onTap: () {},
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -105,26 +134,7 @@ class SignInPage extends GetView<SignInController> {
           SizedBox(
             height: 35.h,
           ),
-          Column(
-            children: [
-              Text(
-                "Already have an account",
-                style: TextStyle(
-                    color: AppColors.primaryText,
-                    fontWeight: FontWeight.normal,
-                    fontSize: 12.sp),
-              ),
-              GestureDetector(
-                child: Text(
-                  "Sign up here",
-                  style: TextStyle(
-                      color: AppColors.primaryElement,
-                      fontWeight: FontWeight.normal,
-                      fontSize: 12.sp),
-                ),
-              )
-            ],
-          )
+          _buildSignupWidget()
         ],
       )),
     );
